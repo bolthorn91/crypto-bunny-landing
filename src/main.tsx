dce7dc59477b3f3e2a3a9ccd8b9575de86813d8c
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2023-06-21 21:57:55 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { routes } from './routes.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import './assets/fonts//Spartan-Regular.ttf';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
